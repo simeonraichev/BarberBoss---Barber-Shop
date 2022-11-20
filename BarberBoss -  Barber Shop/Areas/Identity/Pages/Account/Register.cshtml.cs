1f865444bfd0c135118fee9576ea999c6a23a3ce
Simeon Raychev <simeon0022@gmail.com>
2022-11-20 23:34:58 +0200

    Staged Home View

--- BarberBoss -  Barber Shop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BarberBoss -  Barber Shop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -24,27 +24,24 @@ namespace BarberBoss____Barber_Shop.Areas.Identity.Pages.Account
     using Microsoft.AspNetCore.WebUtilities;
     using Microsoft.Extensions.Logging;
 
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<MyApplicationUser> signInManager;
-        private readonly UserManager<MyApplicationUser> userManager;
-        private readonly IUserStore<MyApplicationUser> userStore;
-        private readonly IUserEmailStore<MyApplicationUser> emailStore;
-        private readonly ILogger<RegisterModel> logger;
-        private readonly IEmailSender emailSender;
+        private readonly SignInManager<MyApplicationUser> _signInManager;
+        private readonly UserManager<MyApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<MyApplicationUser> userManager,
-            IUserStore<MyApplicationUser> userStore,
             SignInManager<MyApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
-            this.userManager = userManager;
-            this.userStore = userStore;
-            this.signInManager = signInManager;
-            this.logger = logger;
-            this.emailSender = emailSender;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -53,7 +50,6 @@ namespace BarberBoss____Barber_Shop.Areas.Identity.Pages.Account
         public string ReturnUrl { get; set; }
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
 
         public class InputModel
         {
@@ -68,13 +64,6 @@ namespace BarberBoss____Barber_Shop.Areas.Identity.Pages.Account
             [RegularExpression(@"[A-Za-z]+", ErrorMessage = "The {0} must contain only letters.")]
             [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             public string LastName { get; set; }
-
-            [Required]
-            [Display(Name = "Username")]
-            [RegularExpression(@"^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$", ErrorMessage = "Invalid Username.")]
-            [StringLength(15, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
-            public string UserName { get; set; }
-
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -88,54 +77,46 @@ namespace BarberBoss____Barber_Shop.Areas.Identity.Pages.Account
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "Passwords does not match")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-
         }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
-            ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
-            ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            returnUrl = returnUrl ?? Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = CreateUser();
-                user.FirstName = Input.FirstName;
-                user.LastName = Input.LastName;
-
-                await userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
-                await emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await userManager.CreateAsync(user, Input.Password);
-
+                var user = new MyApplicationUser { UserName = Input.Email, FirstName = Input.FirstName,LastName = Input.LastName, Email = Input.Email };
+                var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("User created a new account with password.");
 
-                    var userId = await userManager.GetUserIdAsync(user);
-                    var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { area = "Identity", userId, code, returnUrl },
+                        values: new { area = "Identity", userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (userManager.Options.SignIn.RequireConfirmedAccount)
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl });
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
                     }
                     else
                     {
-                        await signInManager.SignInAsync(user, isPersistent: false);
+                        await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
                 }
@@ -147,30 +128,6 @@ namespace BarberBoss____Barber_Shop.Areas.Identity.Pages.Account
 
             // If we got this far, something failed, redisplay form
             return Page();
-        }
-
-        private MyApplicationUser CreateUser()
-        {
-            try
-            {
-                return Activator.CreateInstance<MyApplicationUser>();
-            }
-            catch
-            {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(MyApplicationUser)}'. " +
-                    $"Ensure that '{nameof(MyApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
-            }
-        }
-
-        private IUserEmailStore<MyApplicationUser> GetEmailStore()
-        {
-            if (!userManager.SupportsUserEmail)
-            {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
-            }
-
-            return (IUserEmailStore<MyApplicationUser>)userStore;
         }
     }
 }
