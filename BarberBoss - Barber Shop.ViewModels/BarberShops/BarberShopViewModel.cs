4ffd066213f4e9fead4705f91754d30b4440e46a
Simeon Raychev <simeon0022@gmail.com>
2022-11-23 22:24:32 +0200

    BarberShop ViewModel

--- BarberBoss - Barber Shop.ViewModels/BarberShops/BarberShopViewModel.cs
+++ BarberBoss - Barber Shop.ViewModels/BarberShops/BarberShopViewModel.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace BarberBoss___Barber_Shop.ViewModels.BarberShops
 {
-    internal class BarberShopViewModel
+    public class BarberShopViewModel
     {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string TownName { get; set; }
+
+        public string Address { get; set; }
     }
 }
