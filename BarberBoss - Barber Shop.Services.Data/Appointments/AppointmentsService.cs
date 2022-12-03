fdd78440e6bb12eb17c7a65505d2f273c360448d
Simeon Raychev <simeon0022@gmail.com>
2022-12-03 15:05:26 +0200

    AppointmentService Interface

--- BarberBoss - Barber Shop.Services.Data/Appointments/AppointmentsService.cs
+++ BarberBoss - Barber Shop.Services.Data/Appointments/AppointmentsService.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace BarberBoss___Barber_Shop.Services.Data.Appointments
 {
-    internal class AppointmentsService
+    internal class AppointmentsService : IAppointmentsService
     {
+        public Task AddAsync(string userId, string barberShopId, int serviceId, DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ConfirmAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeclineAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllByBarberShopAsync<T>(string salonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync<T>(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetNextClientByUserAsync<T>(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetPastClietByUserAsync<T>(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RateAppointmentAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BarberBoss - Barber Shop.Services.Data/Appointments/IAppointmentsService.cs
+++ BarberBoss - Barber Shop.Services.Data/Appointments/IAppointmentsService.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace BarberBoss___Barber_Shop.Services.Data.Appointments
 {
-    internal class IAppointmentsService
+    public interface IAppointmentsService
     {
+        Task<T> GetByIdAsync<T>(string id);
+
+        Task<IEnumerable<T>> GetAllAsync<T>();
+
+        Task<IEnumerable<T>> GetAllByBarberShopAsync<T>(string salonId);
+
+        Task<IEnumerable<T>> GetNextClientByUserAsync<T>(string userId);
+
+        Task<IEnumerable<T>> GetPastClietByUserAsync<T>(string userId);
+
+        Task AddAsync(string userId, string barberShopId, int serviceId, DateTime dateTime);
+
+        Task DeleteAsync(string id);
+
+        Task ConfirmAsync(string id);
+
+        Task DeclineAsync(string id);
+
+        Task RateAppointmentAsync(string id);
     }
 }
