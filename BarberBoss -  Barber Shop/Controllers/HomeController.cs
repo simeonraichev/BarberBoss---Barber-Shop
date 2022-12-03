e1bc5d7d9387fe45b3d49b7a16ad410d345c2eda
Simeon Raychev <simeon0022@gmail.com>
2022-12-03 13:46:10 +0200

    HomeController changed

--- BarberBoss -  Barber Shop/Controllers/HomeController.cs
+++ BarberBoss -  Barber Shop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using BarberBoss___Barber_Shop.Web.ViewModels;
+﻿using BarberBoss___Barber_Shop.Common;
+using BarberBoss___Barber_Shop.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,27 +7,41 @@ namespace BarberBoss____Barber_Shop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            
+            return this.View();
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
+        }
+
+        [Route("/Home/Error/404")]
+        public IActionResult Error404()
+        {
+            return this.View();
+        }
+
+        [Route("/Home/Error/{code:int}")]
+        public IActionResult Error(int code)
+        {
+            // Could handle different codes here
+            // or just return the default error view
+            return this.View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(
+                new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
