7b02b629be71f9e6c8611376523a0d5a85051b76
Simeon Raychev <simeon0022@gmail.com>
2022-12-03 14:23:26 +0200

    AppoinmentsViewModel added

--- BarberBoss - Barber Shop.ViewModels/Appointments/AppointmentsViewModel.cs
+++ BarberBoss - Barber Shop.ViewModels/Appointments/AppointmentsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BarberBoss____Barber_Shop.Services.Mapping;
+using BarberBoss___Barber_Shop.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace BarberBoss___Barber_Shop.ViewModels.Appointments
 {
-    internal class AppointmentsViewModel
+    public class AppointmentsViewModel : IMapFrom<Appointment>
     {
+        public string Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string BarberShopId { get; set; }
+
+        public string BarberShopName { get; set; }
+
+        public string BarberShopTownName { get; set; }
+
+        public string BarberShopAddress { get; set; }
+
+        public int ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+        //TODO: Add functionality if the appointment is confirmed
+        //TODO: Add functionality if the use has rated the barberShop
     }
 }
