59e55c6f4b68cbd1f848c02b4c05dc429cf92c31
Simeon Raychev <simeon0022@gmail.com>
2022-12-06 22:53:15 +0200

    ApplicationUserConfiguration added

---
+++ BarberBoss - Barber Shop.Data/Configuration/MyApplicationUserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using BarberBoss___Barber_Shop.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberBoss___Barber_Shop.Data.Configuration
+{
+    public class MyApplicationUserConfiguration : IEntityTypeConfiguration<MyApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<MyApplicationUser> appUser)
+        {
+            appUser
+                .HasMany(e => e.Claims)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            appUser
+                .HasMany(e => e.Logins)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            appUser
+                .HasMany(e => e.Roles)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
