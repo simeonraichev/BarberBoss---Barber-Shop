48b4e59daab7eb9f3e96a810014cf6c9e6833782
Simeon Raychev <simeon0022@gmail.com>
2022-12-07 19:16:16 +0200

    TownViewModels intialized

---
+++ BarberBoss - Barber Shop.ViewModels/Towns/TownInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberBoss___Barber_Shop.ViewModels.Towns
+{
+    public class TownInputModel
+    {
+        [Required]
+        [StringLength(
+            40,
+            ErrorMessage = "Name must be between 2 and 40 characters.",
+            MinimumLength = 1)]
+        public string Name { get; set; }
+    }
+}
---
+++ BarberBoss - Barber Shop.ViewModels/Towns/TownsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberBoss___Barber_Shop.ViewModels.Towns
+{
+    public class TownsListViewModel
+    {
+        public IEnumerable<TownViewModel> Towns { get; set; }
+    }
+}
