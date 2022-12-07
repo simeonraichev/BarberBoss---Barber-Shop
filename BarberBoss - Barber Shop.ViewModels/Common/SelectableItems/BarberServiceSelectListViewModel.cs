098c7fe6115f0c91ea4406cfc7ccaf7b1951f365
Simeon Raychev <simeon0022@gmail.com>
2022-12-07 19:23:06 +0200

    Selectable Items View Models added

---
+++ BarberBoss - Barber Shop.ViewModels/Common/SelectableItems/BarberServiceSelectListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberBoss___Barber_Shop.ViewModels.Common.SelectableItems
+{
+    public class BarberServiceSelectListViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
